024fa83cfa9fa73244824aa58bb8b25295362b4a
Emufighter <nhinds1988@gmail.com>
2020-01-30 17:01:23 -0800

    Added alien bullet interaction

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -45,7 +45,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet positions. 
     bullets.update()
@@ -54,6 +54,38 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    # check for any bullets that have hit aliens. 
+    # If so, get rid of the bullet and the alien. 
+
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """Respond to bullet-alien collions."""
+    # Remove any bullets and aliens that have collided. 
+    collisions = pygame.sprite.pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # Destroy existing bullets and create new fleet. 
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
+def update_aliens(ai_settings, aliens):
+    """Check if the fleet is at an edge, and then update the positions of all aliens in the fleet."""
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+    """Respond appropiately if any aliens have reached an edge."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet and change the fleet's direction."""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet."""
